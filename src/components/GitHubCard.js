38cb9e2cc6df7639a23183bcc138721d3e0e3cbf
CaitlinStuder <studercaitlin98@gmail.com>
2022-09-20 18:01:12 -0400

    snapshot test success

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SamplePic from "../SamplePic.png";  
+
+function GitHubCard() {
+  return (
+    <div data-testId="gitHubCard-1">
+      <div className="card">React Snapshot tests - GitHub Mockup</div>
+      <img
+        styles={{ minWidth: 300, minHeight: 300 }}
+        src={SamplePic}
+        alt="Card cap"
+      />
+      <div className="card-body">
+        <h3><a href="https://github.com/CaitlinStuder">My GitHub</a></h3>
+        <p className="card-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non,
+          nesciunt. 
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default GitHubCard;
